6e775673af26c48ebbd235da6c044c18d8037f2f
Rabbe <onikmahmudrabbe@gmail.com>
2022-11-09 04:00:59 +0600

    env added

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCuNnrH5MvhtEz3zGRNSq8CqBHr6Eq5mk0",
-    authDomain: "my-awesome-clicks.firebaseapp.com",
-    projectId: "my-awesome-clicks",
-    storageBucket: "my-awesome-clicks.appspot.com",
-    messagingSenderId: "168839613774",
-    appId: "1:168839613774:web:0782e21698be976abaa21f"
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
