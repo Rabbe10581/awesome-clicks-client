5418ac97a6742da18557b47d82c4260ee68cae02
Rabbe <onikmahmudrabbe@gmail.com>
2022-11-10 02:13:50 +0600

    topics card and service card added

--- src/Pages/Home/Home/Topics/TopicsCard.js
+++ src/Pages/Home/Home/Topics/TopicsCard.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const TopicsCard = ({ topic }) => {
-    const { _id, img, price, title } = topic;
+    const { _id, img, price, title, details } = topic;
     return (
         <div className="card card-compact w-96 bg-base-100 shadow-xl">
             <figure><img src={img} alt="Shoes" /></figure>
             <div className="card-body">
                 <h2 className="card-title text-3xl font-bold">{title}</h2>
                 <p className='text-2xl text-orange-600 font-semibold'>Price: <span>${price}</span></p>
+                <p>{details.slice(0, 100)}...</p>
                 <div className="card-actions justify-end">
                     <Link to={`/checkout/${_id}`}>
                         <button className="btn btn-secondary text-white">Checkout</button>
