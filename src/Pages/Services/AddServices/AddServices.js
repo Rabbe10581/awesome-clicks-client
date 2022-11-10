e2c18ad14d4544c7c6d081e5f3547e7cb0293508
Rabbe <onikmahmudrabbe@gmail.com>
2022-11-10 22:37:05 +0600

    Add services editted

--- src/Pages/Services/AddServices/AddServices.js
+++ src/Pages/Services/AddServices/AddServices.js
@@ -1,17 +1,59 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { toast, ToastContainer } from 'react-toastify';
 
 const AddServices = () => {
+    const [insertServices, setInsertServices] = useState([]);
+    useEffect(() => {
+        fetch('http://localhost:5000/services')
+            .then(res => res.json())
+            .then(data => setInsertServices(data))
+    }, []);
+
     const handleServiceInsert = event => {
-        event.preventDefault()
+        event.preventDefault();
+        const form = event.target;
+        const name = form.name.value;
+        const price = form.price.value;
+        const details = form.details.value;
+        const img = form.img.value;
+        const insertServices = {
+            name,
+            price,
+            details,
+            img,
+
+        }
+        fetch('http://localhost:5000/service', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(insertServices)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                if (data.acknowledged) {
+                    toast('Service Added')
+                    form.reset();
+
+                }
+            })
+            .catch(er => console.error(er));
     }
     return (
-        <div>
+        <div className='text-center my-16'>
+            <h2 className="text-5xl my-4">Set a new service here</h2>
             <form onSubmit={handleServiceInsert}>
-                <input type="text" name='name' placeholder="Type here" className="input input-bordered input-primary w-full max-w-xs" />
-                <input type="text" name='price' placeholder="Type here" className="input input-bordered input-primary w-full max-w-xs" /><br />
-                <input type="text" name='details' placeholder="Type here" className="input input-bordered input-primary w-full max-w-xs" />
-                <input type="text" name='img' placeholder="Type here" className="input input-bordered input-primary w-full max-w-xs" />
+                <div className=''>
+                    <input type="text" name='name' placeholder="Service Title" className="input input-bordered input-primary w-full max-w-xs" /><br />
+                    <input type="text" name='price' placeholder="price" className="input input-bordered input-primary w-full max-w-xs my-2" /><br />
+                    <input type="text" name='details' placeholder="Details" className="input input-bordered input-primary w-full max-w-xs" /><br />
+                    <input type="text" name='img' placeholder="Image URL" className="input input-bordered input-primary w-full max-w-xs my-2" /><br />
+                    <button className="btn btn-secondary">Insert Service</button>
+                </div>
             </form>
+            <ToastContainer></ToastContainer>
         </div >
     );
 };
